9fb47836a00060b031c3a7ef30fc1079f43d1744
hnujwal <ujwalhn@gmail.com>
2025-04-15 23:33:55 +0530

    performing arithmetic opretion

---
+++ arithmetic_operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int a,b;
+int main()
+{
+    /* code */
+    printf("enter the two number");
+    scanf("%d%d",&a,&b);
+    printf("addtion of two number:%d\n",a+b);
+    printf("sub of two number=%d\n",a-b);
+    printf("mul of two number=%d\n",a*b);
+    printf("div of two number=%d",a/b);
+    return 0;
+}
